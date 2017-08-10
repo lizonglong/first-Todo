9cc21da807a4cfd61fc8a7ca1018dd64187cec70
lizonglong <1968339140@qq.com>
2017-08-10 19:06:23 +0800

    初步完成

--- build/js/index.js
+++ build/js/index.js
@@ -16,6 +16,7 @@ $addTask.on("submit",function (ev) {
 function init() {
     tableList=store.get("xx") || [];
     createHtml()
+    clock()
 }
 
 //存数据
@@ -29,16 +30,34 @@ function createHtml(){
     var $taskList=$(".task-list");
     // console.log(tableList.length)
     $taskList.html(null)
+    var complate_items=[];
     for(var i=0;i<tableList.length;i++){
-        $taskList.prepend(bindHtml(tableList[i],i))
+        if(tableList[i].tick){
+            complate_items[i]=tableList[i];
+        }
+        else{
+            var $item=bindHtml(tableList[i],i);
+            $taskList.prepend($item)
+            clock($item)
+        }
+    }
+
+    for(var j=0;j<complate_items.length;j++){
+        if(complate_items[j]){
+            var $item=bindHtml(complate_items[j],j)
+            $item.addClass("deleteline")
+            // if(!$item)continue;
+            $taskList.append($item)
+        }
+
     }
     bindDelete();//
     bindDetail()
-     tick()
+     tick()     //点击checkbox
 }
 //绑定html
 function bindHtml(data,index) {
-    var str='<li data-index="'+index+'">'+
+    var str='<li data-index="'+index+'" >'+
                 '<input type="checkbox" class="complate" '+(data.tick? "checked" :"")+'>'+
                 '<p class="content">'+data.content+'</p>'+
                 '<div class="right">'+
@@ -46,18 +65,31 @@ function bindHtml(data,index) {
                 '<span class="detail r-main">详细</span>'+
                 '</div>'+
             '</li>'
-    return str;
+    return $(str);     //传出对应的li,只有jQuery对象，给li加class才不会报错
 }
 
-/*---------------------------删除------------------------------*/
+/*---------------------------删除 start------------------------------*/
 //点击事件---删除功能---更新本地存储
 //点击事件
 function bindDelete() {
     $(".delete.r-main").on("click",function () {
-        var off=confirm("确定删除？");
-        if(!off)return;
-        var index=$(this).parent().parent().data("index");
-        removeTask(index)
+        var $that=$(this)
+        $(".Alert").show()
+        // var off=false;
+        $(".primary.confirm").bind("click",function () {//记得解绑
+            // off=true;
+            $(".Alert").hide()
+            // if(!off)return;
+            var index=$that.parent().parent().data("index");
+            removeTask(index)
+            $(".primary.confirm").unbind("click");
+        })
+        $(".cancel").click(function () {
+            // off=false;
+            $(".Alert").hide()
+        })
+        // var off=confirm("确定删除？");
+
     })
 }
 //删除功能
@@ -72,7 +104,7 @@ function refresh() {
 }
 /*---------------------------删除 end------------------------------*/
 
-/*---------------------------生成详细 start------------------------------*/
+/*---------------------------生成详细 start------------------------*/
 //点击事件,获取index---生成---删除
     function bindDetail() {
         $(".detail.r-main").on("click",function () {
@@ -95,7 +127,7 @@ function refresh() {
             '</div>'+
             '<div class="remind input-item">'+
             '<label for="b">提醒时间</label>'+
-            '<input id="b" class="datetime" type="date" value="'+data.datetime+'">'+
+            '<input id="b" class="datetime" type="text" value="'+(data.datetime?data.datetime:"")+'">'+
             '</div>'+
             '<div class="input-item">'+
             '<button>更新</button>'+
@@ -103,7 +135,13 @@ function refresh() {
             '<div class="close">X</div>'+
             '</form>'+
             '</div>'
+
         $(".container").append(str);
+
+        //日期插件
+        $.datetimepicker.setLocale('ch');//设置中文
+        $('.datetime').datetimepicker();
+
         removeDetail();//删除详细
         updateDetail(index);//更新详细
         dbDetail();     //双击
@@ -116,7 +154,7 @@ function refresh() {
     }
 /*---------------------------生成详细 end------------------------------*/
 
-/*---------------------------更新详细 end------------------------------*/
+/*---------------------------更新详细 start----------------------------*/
     //点击，获取index
     function updateDetail(index){
         $(".up-task").on("submit",function (ev) {
@@ -155,11 +193,12 @@ function refresh() {
         store.set("xx",tableList)
         //createDetail(tableList[index],index)
     }
-    /*---------------------------更新详细 end------------------------------*/
+    /*---------------------------更新详细 end--------------------------*/
 
     /*---------------------------checked-------------------------------*/
     function tick() {
         var $check=$(".complate");
+        var $alist=$(".container .task-list li");
         $check.on("click",function(){
              var index=$(this).parent().data("index");
              if(!tableList[index].tick){
@@ -168,8 +207,50 @@ function refresh() {
              else{
                  update({tick:false},index)
              }
+             createHtml()       //更新complate
         })
     }
-    /*---------------------------checked-------------------------------*/
+
+    /*---------------------------checked end---------------------------*/
+
+    /*---------------------------clock start--------------------------*/
+
+    function clock(obj) {
+         // console.log($(obj))
+        if(!$(obj)[0]) return;
+         clearInterval($(obj)[0].timer);
+        $(obj)[0].timer=setInterval(function () {
+            var startTime=new Date().getTime();//获取开始时间
+               // console.log(tableList.length)
+            for(var i=0;i<tableList.length;i++){
+                // console.log(!tableList[i].datetime,1)
+                //过滤
+                if(tableList[i].tick||!tableList[i].datetime||tableList[i].off)continue;
+                var endTime=new Date(tableList[i].datetime).getTime();//获取结束时间
+                if(endTime-startTime<1){
+                    clearInterval($(obj)[0].timer);
+                    playmusic();     //播放音乐
+                    showAlert(i);     //弹出提醒框
+                }
+            }
+            // clearInterval($(obj)[0].timer);
+        },1000)
+
+    }
+    //播放音乐
+    function playmusic() {
+        var music=document.getElementById("music");
+        music.play()
+    }
+    //
+    function showAlert(i) {
+        $(".msg").show();
+        $(".msg-content").text(tableList[i].content)
+        $(".msg-btn").click(function () {
+            update({off:true},i)
+            $(".msg").hide();
+        })
+    }
+    /*----------------------------clock end---------------------------*/
 
 }())
